f703d2d30a2dd2fdc1ffe1e595a4bdfe545c2f3f
brkyozdemir <berkayozdemir02@gmail.com>
2020-08-15 22:19:13 +0300

    updates for docker

--- src/hooks/populate-user.js
+++ src/hooks/populate-user.js
@@ -1,7 +1,3 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-
-// eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
     const { app, method, result, params } = context;
